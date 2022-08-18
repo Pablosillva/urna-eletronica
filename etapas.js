dbe8d69a116cf3b7059b216cb8e66fb08f3bfb8c
Pablo Duarte <ptox.cs@hotmail.com>
2022-08-17 22:11:36 -0300

    Projeto Urna

---
+++ etapas.js
@@ -0,0 +1,287 @@
+let etapas = [
+//    presidentes
+
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'CIRO GOMES',
+                partido: 'PDT',
+                vice: 'ANA PAULA MATOS',
+                fotos:[
+                    {url:'ciro_gomes.jpg', legenda: 'Presidente'},
+                    {url:'ana_paula_matos.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '27',
+                nome: 'CONSTITUINTE EYMAEL',
+                partido: 'DC',
+                vice: 'PROFESSOR BRAVO',
+                fotos:[
+                    {url:'constituinte_eymael.jpeg', legenda: 'Presidente'},
+                    {url:'professor_bravo.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '30',
+                nome: "FELIPE D'AVILA",
+                partido: 'NOVO',
+                vice: 'TIAGO MITRAUD',
+                fotos:[
+                    {url:'felipe_davila.jpg', legenda: 'Presidente'},
+                    {url:'tiago_mitraud.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'JAIR BOLSONARO',
+                partido: 'PL',
+                vice: 'BRAGA NETTO',
+                fotos:[
+                    {url:'jair_bolsonaro.jpeg', legenda: 'Presidente'},
+                    {url:'braga_netto.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '80',
+                nome: 'LÉO PÉRICLES',
+                partido: 'UP',
+                vice: 'SAMARA MARTINS',
+                fotos:[
+                    {url:'leo_pericles.jpg', legenda: 'Presidente'},
+                    {url:'samara_martins.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'LULA',
+                partido: 'PT',
+                vice: 'GERALDO ALCKMIN',
+                fotos:[
+                    {url:'lula.jpg', legenda: 'Presidente'},
+                    {url:'geraldo_alckmin.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '90',
+                nome: 'PABLO MARÇAL',
+                partido: 'PROS',
+                vice: 'FÁTIMA PÉROLA NEGRA',
+                fotos:[
+                    {url:'pablo_marçal.jpg', legenda: 'Presidente'},
+                    {url:'fatima_perola.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '14',
+                nome: 'ROBERTO JEFFERSON',
+                partido: 'PTB',
+                vice: 'PADRE KELMON',
+                fotos:[
+                    {url:'roberto_jefferson.jpeg', legenda: 'Presidente'},
+                    {url:'padre_kelmon.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'SIMONE TEBET',
+                partido: 'MDB',
+                vice: 'MARA GABRILLI',
+                fotos:[
+                    {url:'simone_tebet.jpg', legenda: 'Presidente'},
+                    {url:'mara_gabrilli.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'SOFIA MANZANO',
+                partido: 'PCB',
+                vice: 'ANTONIO ALVES',
+                fotos:[
+                    {url:'sofia_manzano.jpeg', legenda: 'Presidente'},
+                    {url:'antonio_alves.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'SORAYA THRONICKE',
+                partido: 'UNIÃO',
+                vice: 'MARCOS CINTRA',
+                fotos:[
+                    {url:'soraya_thronicke.jpg', legenda: 'Presidente'},
+                    {url:'marcos_cintra.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '16',
+                nome: 'VERA',
+                partido: 'PSTU',
+                vice: 'RAQUEL TREMEMBÉ',
+                fotos:[
+                    {url:'vera.jpg', legenda: 'Presidente'},
+                    {url:'raquel_tremembe.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+    },
+    // governador
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '50',
+                nome: 'ADJANY SIMPLICIO',
+                partido: 'PSOL',
+                vice: 'JARDEL QUEIROZ',
+                fotos:[
+                    {url:'adjany_simplicio.jpg', legenda: 'Governador'},
+                    {url:'jardel_queiroz.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '29',
+                nome: 'ADRIANO TRAJANO',
+                partido: 'PCO',
+                vice: 'JOSE PESSOA',
+                fotos:[
+                    {url:'adriano_trajano.jpg', legenda: 'Governador'},
+                    {url:'jose_pessoa.jpeg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '40',
+                nome: 'JOÃO',
+                partido: 'PSB',
+                vice: 'LUCAS RIBEIRO',
+                fotos:[
+                    {url:'joao.jpg', legenda: 'Governador'},
+                    {url:'lucas_ribeiro.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '28',
+                nome: 'MAJOR FÁBIO',
+                partido: 'PRTB',
+                vice: 'DR. JOD CANDEIA',
+                fotos:[
+                    {url:'major_fabio.jpg', legenda: 'Governador'},
+                    {url:'jod_candeia.jpeg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '16',
+                nome: 'NASCIMENTO',
+                partido: 'PSTU',
+                vice: 'ALICE MACIEL',
+                fotos:[
+                    {url:'nascimento.jpg', legenda: 'Governador'},
+                    {url:'alice_maciel.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'NILVAN FERREIRA',
+                partido: 'PL',
+                vice: 'ARTUR BOLINHA',
+                fotos:[
+                    {url:'nilvan_ferreira.jpeg', legenda: 'Governador'},
+                    {url:'artur_bolinha.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '45',
+                nome: 'PEDRO CUNHA LIMA',
+                partido: 'PSDB',
+                vice: 'DOMICIANO CABRAL',
+                fotos:[
+                    {url:'pedro_cunha.jpg', legenda: 'Governador'},
+                    {url:'domiciano_cabral.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'VENEZIANO',
+                partido: 'MDB',
+                vice: 'MAÍSA CARTAXO',
+                fotos:[
+                    {url:'veneziano.jpg', legenda: 'Governador'},
+                    {url:'maisa_cartaxo.jpg', legenda: 'Vice', small: true}
+                ]
+            }
+        ]
+    },
+    // senador
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '500',
+                nome: 'ALEXANDRE SOARES',
+                partido: 'PSOL',
+                fotos:[
+                    {url:'alexandre_soares.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '500',
+                nome: 'ALEXANDRE SOARES',
+                partido: 'PSOL',
+                fotos:[
+                    {url:'alexandre_soares.jpg', legenda: 'Senador'}
+                ]
+            }
+        ]
+    },
+    // deputado estadual
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '13777',
+                nome: 'ABRAAO LEITE',
+                partido: 'PT',
+                fotos:[
+                    {url:'abraao_leite.jpeg', legenda: 'Dep. Estadual'}
+                ]
+            },
+            {
+                numero: '13777',
+                nome: 'ABRAAO LEITE',
+                partido: 'PT',
+                fotos:[
+                    {url:'abraao_leite.jpeg', legenda: 'Dep. Estadual'}
+                ]
+            }
+        ]
+    },
+    // deputado federal 
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '3590',
+                nome: 'ADAILTO BARROS',
+                partido: 'PMB',
+                fotos:[
+                    {url:'adailto_barros.jpg', legenda: 'Dep. Federal'}
+                ]
+            },
+            {
+                numero: '3590',
+                nome: 'ADAILTO BARROS',
+                partido: 'PMB',
+                fotos:[
+                    {url:'adailto_barros.jpg', legenda: 'Dep. Federal'}
+                ]
+            }
+        ]
+    }
+]
